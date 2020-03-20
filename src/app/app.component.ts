8d8a06e78abf1f9a3a5cd33bb1fc12b086a45adf
wchamara <chamara.weerasinghe@my.sliit.lk>
2020-03-21 00:51:53 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'ps';
+  password = "";
+  inputLenth = 0;
+  includeLetters = false;
+  includeNumber = false;
+  includeSymbols = false;
+
+  onChangeLength(value: string) {
+    const pasrsedValue = parseInt(value);
+    if (!isNaN(pasrsedValue)) {
+      this.inputLenth = pasrsedValue;
+    }
+  }
+
+  onButtonClick() {
+    const Characters = "asdfghjklzxcvbnmqwertyuiop";
+    const Numbers = "1234567890";
+    const Symbols = "!@#$%^&*()";
+    let validChars = "";
+
+    if (this.includeLetters) {
+      validChars += Characters;
+    }
+    if (this.includeNumber) {
+      validChars += Numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += Symbols;
+    }
+
+    let generatedPassword = "";
+    for (let i = 0; i < this.inputLenth; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
+  }
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumber() {
+    this.includeNumber = !this.includeNumber;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
 }
